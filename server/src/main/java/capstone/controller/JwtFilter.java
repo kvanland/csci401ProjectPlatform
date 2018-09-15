e02e74810345fa7f111d71e05f853dc446049e42
Kyle Van Landingham <kylevanlandingham9@gmail.com>
2018-09-14 17:17:36 -0700

    Fixed CORS errors with the JWTFilter so requests can be made to the server-side

--- server/src/main/java/capstone/controller/JwtFilter.java
+++ server/src/main/java/capstone/controller/JwtFilter.java
@@ -26,12 +26,18 @@ public class JwtFilter extends GenericFilterBean {
 
 		final HttpServletRequest request = (HttpServletRequest) req;
 		final HttpServletResponse response = (HttpServletResponse) res;
-		final String authHeader = request.getHeader("authorization");
+		final String authHeader = request.getHeader("Authorization");
 
+		response.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+		response.addHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+		response.addHeader("Access-Control-Allow-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range,Authorization");
+		
 		if ("OPTIONS".equals(request.getMethod())) {
-			response.setStatus(HttpServletResponse.SC_OK);
+			response.addHeader("Access-Control-Max-Age", "1728000");
+			response.setStatus(HttpServletResponse.SC_NO_CONTENT);
 			chain.doFilter(req, res);
 		} else {
+			response.addHeader("Access-Control-Expose-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range");
 			if (authHeader == null || !authHeader.startsWith("Bearer ")) {
 				throw new ServletException("Missing or invalid Authorization header");
 			}
